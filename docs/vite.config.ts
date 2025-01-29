9995779495f46e0c635a5850239e34d73bba1c9e
Owais Kamdar <owaiskamdar@Owaiss-MBP.wired.duke.local>
2025-01-29 10:24:21 -0500

    Update Vite config for GitHub Pages

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -3,8 +3,12 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', 
   plugins: [react()],
+  build: {
+    outDir: 'docs', 
+  },
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ['lucide-react'], // Exclude 'lucide-react' from optimization
   },
 });
