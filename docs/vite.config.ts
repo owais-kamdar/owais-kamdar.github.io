7dbdb853387e5686c771e7799fd17cb818f9353a
Owais Kamdar <owaiskamdar@Owaiss-MBP.attlocal.net>
2025-01-29 17:58:12 -0500

    Update Vite config for GitHub Pages

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -3,12 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './', 
+  base: '/portfolio-flask/', // Add a leading slash to the base path
   plugins: [react()],
   build: {
-    outDir: 'docs', 
-  },
-  optimizeDeps: {
-    exclude: ['lucide-react'], // Exclude 'lucide-react' from optimization
+    outDir: 'docs', // Ensure the build output goes into the 'docs' folder
   },
 });
