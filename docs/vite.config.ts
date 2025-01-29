244d71f4328150c07e19b2c3b8b8be19f151f2f8
Owais Kamdar <owaiskamdar@Owaiss-MBP.attlocal.net>
2025-01-29 18:24:04 -0500

    Fix build output to overwrite docs folder

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
 export default defineConfig({
-  base: '/', // Set base path to root
+  base: '/',
   plugins: [react()],
   build: {
-    outDir: 'docs', // Output the build files into the 'docs' folder
+    outDir: '.', // Output the build files into the current directory (docs)
   },
 });
