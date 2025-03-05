793a0e65a2d9f7a13c0298c65c38650c14a8fd10
Owais Kamdar <owais.kamdar@duke.edu>
2025-03-05 09:13:13 -0500

    fixed directory

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -4,6 +4,6 @@ export default defineConfig({
   base: '/',
   plugins: [react()],
   build: {
-    outDir: 'dist', // Output the build files into the current directory (docs)
+    outDir: 'docs', // Output the build files into the current directory (docs)
   },
 });
