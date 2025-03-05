53d31d7308a6ea077c0e57b0a28bdc421ef2fad9
Owais Kamdar <owais.kamdar@duke.edu>
2025-03-05 09:43:27 -0500

    update

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
-  base: '/',
+  base: '/owais-kamdar.github.io/',
   plugins: [react()],
   css: {
     modules: {
       localsConvention: "camelCase"
     },
+  },
+  build: {
+    outDir: 'dist',
   },
 })
