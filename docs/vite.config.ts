ab15ab19953695efcb1bff35daab022fa2047588
Owais Kamdar <owais.kamdar@duke.edu>
2025-07-28 19:50:38 -0700

    updated projects

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
-  base: '/portfolio-pages/',
+  base: '/',
   plugins: [react()],
   build: {
     outDir: 'dist',
